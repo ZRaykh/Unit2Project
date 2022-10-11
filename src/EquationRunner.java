98d8191a92cbe681089725e174699ac40ca48c45
BT_4N2_30 <zraykh2784@bths.edu>
2022-10-11 12:32:01 -0400

    Started collecting user input, created all Java files

---
+++ src/EquationRunner.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+public class EquationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        //Collecting initial coordinate points in (x,y) format
+        System.out.println("Please input your first set of coordinate points in the form of \" (x,y0\" :");
+        String pointOne = s.nextLine();
+        System.out.println("Please input your second set of coordinate points in the form of \" (x,y0\" :");
+        String pointTwo = s.nextLine();
+
+        //Isolating the coordinate points into xOne, yOne, xTwo, and yTwo
+
+
+    }
+}
