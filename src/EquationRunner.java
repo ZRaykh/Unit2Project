0b8e38c4259728b6f1dc9724ae20367084cd7298
BT_4N2_30 <zraykh2784@bths.edu>
2022-10-12 12:28:44 -0400

    Created a program to calculate linear equations, the distance between two points, the slope, and the y intercept.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,16 +1,26 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
         //Collecting initial coordinate points in (x,y) format
-        System.out.println("Please input your first set of coordinate points in the form of \" (x,y0\" :");
+        System.out.println("Please input your first set of coordinate points in the form of \"(x,y)\":");
         String pointOne = s.nextLine();
-        System.out.println("Please input your second set of coordinate points in the form of \" (x,y0\" :");
+        System.out.println("Please input your second set of coordinate points in the form of \"(x,y)\":");
         String pointTwo = s.nextLine();
 
         //Isolating the coordinate points into xOne, yOne, xTwo, and yTwo
 
+        int comma = pointOne.indexOf(",");
+        double xOne = Double.parseDouble(pointOne.substring(1, comma));
+        double yOne = Double.parseDouble(pointOne.substring(comma + 1, pointOne.length() - 1 ));
 
+        comma = pointTwo.indexOf(",");
+        double xTwo = Double.parseDouble(pointTwo.substring(1, comma));
+        double yTwo = Double.parseDouble(pointTwo.substring(comma + 1, pointTwo.length() - 1 ));
+
+        //Creating new object "equation"
+        LinearEquation equation = new LinearEquation(xOne,yOne,xTwo,yTwo,pointOne,pointTwo);
+        System.out.print(equation);
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,67 @@
+import java.nio.file.DirectoryStream;
+import java.text.DecimalFormat;
 public class LinearEquation {
+
+    DecimalFormat d = new DecimalFormat("0.00");
+    //Double variables initialized
+    private double xOne;
+    private double yOne;
+    private double xTwo;
+    private double yTwo;
+
+    //Coordinate point variables initialized
+    private String pointOne;
+    private String pointTwo;
+
+    //Creating public class: LinearEquation
+    public LinearEquation (double xOne, double yOne, double xTwo, double yTwo, String pointOne, String pointTwo) {
+        this.xOne = xOne;
+        this.yOne = yOne;
+        this.xTwo = xTwo;
+        this.yTwo = yTwo;
+        this.pointOne = pointOne;
+        this.pointTwo = pointTwo;
+    }
+
+    //slope calculator for the program's use | formula: y2 - y1 / x2 - x1
+    public double slopeNumerical(){
+        double slope = (yTwo - yOne) / (xTwo - xOne);
+        return slope;
+    }
+
+    //Slope calculator with a fractional slope format!
+    public String slopeFractional(){
+        double rise = (yTwo - yOne);
+        double run =  (xTwo - xOne);
+        String slopeFraction = d.format(rise) + "/" + d.format(run);
+        return slopeFraction;
+    }
+
+    //Y Intercept calculator via slope formula : y = mx + b | converted to: y - mx = b
+    public double yIntercept(){
+        double yInter = yOne - (slopeNumerical() * xOne);
+        return yInter;
+    }
+
+    //Returns the Slope Intercept Form
+    public String makeEquation(){
+        String equation = "y = " + slopeFractional() + "(x) + " + d.format(yIntercept());
+        return equation;
+    }
+
+    //Returns the distance between two points
+    public String distance(){
+        double distance = Math.sqrt(Math.pow((xTwo - xOne),2) + Math.pow((yTwo - yOne),2));
+        distance = Math.round(distance * 100);
+        distance /= 100;
+        return d.format(distance);
+    }
+
+    //Returns the calculated values, ordered and formatted
+    public String toString(){
+        String output = "First coordinate points: " + pointOne + "\nSecond coordinate points: " + pointTwo
+                + "\nSlope of the line: " + d.format(slopeNumerical()) + "\nY-intercept: " + d.format(yIntercept())
+                + "\nSlope intercept form: " + makeEquation() + "\nDistance between points: " + distance();
+        return output;
+    }
 }
